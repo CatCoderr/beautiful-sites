1dca6be0261bed71a73462fb8548403c16d36340
CatCoderr <catcoderr@gmail.com>
2023-06-11 17:25:53 +0300

    chore: increase length of news title field [back]

--- django_app/news/models.py
+++ django_app/news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.TextField()
     author = models.CharField(max_length=100, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
